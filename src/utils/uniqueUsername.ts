dc865d81f217874304775f8de43b80ded67ab587
emspen <andrewemspen@outlook.com>
2025-05-05 21:45:03 -0500

    fix: fix uniqueUsername util

--- src/utils/uniqueUsername.ts
+++ src/utils/uniqueUsername.ts
@@ -7,14 +7,14 @@ import httpStatus from 'http-status'
 export const generateUniqueUsername = async (email: string): Promise<string> => {
   try {
     const base = slugify(email.split('@')[0], {lower: true, strict: true})
-    let user_name = base
+    let userName = base
     let count = 1
 
-    while (await prisma.user.findUnique({where: {user_name}})) {
-      user_name = `${base}-${count}`
+    while (await prisma.user.findUnique({where: {userName}})) {
+      userName = `${base}-${count}`
       count++
     }
-    return user_name
+    return userName
   } catch (error) {
     logger.error(`Error generating unique username for ${email}:`, error)
     throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Failed to generate unique username')
